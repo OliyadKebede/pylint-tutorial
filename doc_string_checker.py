9ab639d94ed8507f01dac0745fa6fcf25eec2f0d
OliyadKebede <oliyadkebede32@gmail.com>
2023-02-23 04:28:12 -0800

    update checker

--- doc_string_checker.py
+++ doc_string_checker.py
@@ -16,10 +16,10 @@ class TripleDoubleStringChecker(BaseTokenChecker):
     _string_tokens = {}
 
     def process_tokens( self , tokens):
-        for (tok_type, token, (start_row , start_col), _, _) in tokens:
+        for (tok_type, token, (start_row , _), _, _) in tokens:
             if tok_type == tokenize.STRING:
                 token_text = token.strip('"').strip("'")
-                self._string_tokens[token_text] = ( token , start_row , start_col )
+                self._string_tokens[token_text] = ( token , start_row )
 
     def visit_module(self, node):
         self._check_docstring(node)
@@ -33,6 +33,6 @@ class TripleDoubleStringChecker(BaseTokenChecker):
     def _check_docstring( self , node):
 
         if node.doc in self._string_tokens:
-            token , row , col  = self._string_tokens[node.doc]
+            token , row = self._string_tokens[node.doc]
             if not token.startswith('"""'):
-                self.add_message('invalid-module-string-quote',line = row ,col_offset=col)
+                self.add_message('invalid-module-string-quote', line = row )
